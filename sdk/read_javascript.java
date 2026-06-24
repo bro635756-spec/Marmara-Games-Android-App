0d95d103f60464a815527ad1d143edfbaf271a63
burakDev <bro635756@gmail.com>
2026-06-24 14:44:44 +0300

    Add read_javascript class for WebView JS execution

--- sdk/read_javascript.java
+++ sdk/read_javascript.java
@@ -1,1 +1,19 @@
+package com.marmaragames.sdk;
 
+import android.util.Log;
+import android.webkit.WebView;
+
+public class read_javascript {
+    private static final String TAG = "SDK_JS_Reader";
+
+    public static void executeBridgeScript(WebView webView, String scriptPath) {
+        Log.d(TAG, "Loading core JavaScript bridge configuration into WebView context.");
+        // WebView üzerinden js bileşenlerinin yerel tetikleyicilerini bağlar
+        webView.post(new Runnable() {
+            @Override
+            public void run() {
+                webView.evaluateJavascript("javascript:if(typeof HLSController !== 'undefined') HLSController.initializeStream();", null);
+            }
+        });
+    }
+}
